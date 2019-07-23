3c145c062f7eba637394ab8ccf8c591d7dcd734f
Elena <danilova.elena.v@gmail.com>
2019-07-23 11:08:02 +0500

    fixed condition

--- 1. intro/script.js
+++ 1. intro/script.js
@@ -2,18 +2,22 @@ const sum = (x) => {
     let result = 0;
 
     function f(x) {
-        if (x) {
+
+        if (x !== undefined) {
+            x = Number(x);
             result += isNaN(x) ? 0 : x;
 
             return f;
-        } else {
-            return result;
         }
+
+        return result;
     }
 
     return f(x);
 };
 
-console.log(sum(1)(2)(3)(4)(5)());
-console.log( sum('sadfghb')('ref')(3)(4)(5)());
-console.log( sum(1)({})(3)(4)(5)());
+console.log(sum(null)(1)());
+console.log(sum(0)(1)(2)(3)(4)());
+console.log(sum('sadfghb')('ref')(3)(4)(5)());
+console.log(sum(1)({})(3)(4)(5)());
+console.log(sum(1)('')(3)(4)(5)());
